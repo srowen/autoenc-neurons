6073f5f16e1f33ae8c14306ac4bc0c256ced4488
Sean Owen <srowen@gmail.com>
2017-07-28 14:45:35 +0100

    Add docs and further updates

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# autoenc-neurons
+# autoenc-neurons (work in progress)
 
-Download the complete works of Jane Austen with:
+An experiment with convolutional autoencoders as applied to text, and exploring what the
+'neurons' in the convolutional layers respond to.
+
+## Running
 
 ```bash
-curl -O http://www.gutenberg.org/files/31100/31100.txt
+python3 autoenc-neurons.py
 ```
+
+## References
+
+The source text is The Complete Project Gutenberg Works of Jane Austen:                       
+http://www.gutenberg.org/files/31100/31100.txt
+
+For more on convolutional autoencoders in Keras, see:
+https://blog.keras.io/building-autoencoders-in-keras.html
--- autoenc-neurons.py
+++ autoenc-neurons.py
@@ -11,23 +11,20 @@ import tensorflow as tf
 from keras import backend as K
 from keras.layers import Conv1D, Dense, MaxPooling1D, UpSampling1D
 from keras.models import Model, Sequential
-from keras.optimizers import RMSprop
 
 # Cap maximum length of lines, for simplicity. Must be a multiple of pool_size ^ (num pool layers)
 max_len = 80
 
-# 0 for CPU, or >0 for GPUs
-gpu_count = 0
-# Dimension of embedding for individual characters in first, last layesr
+# 0 for CPU, or 1-2 for GPUs
+gpu_count = 2
+# Dimension of embedding for individual characters in first, last layers
 embedding_dim = 32
 # Filter depth of first 1D convolution
-num_filters_1 = 30
+num_filters_1 = 50
 # Filter depth of second 1D convolution
-num_filters_2 = 20
-# Filter depth of third 1D convolution
-num_filters_3 = 10
+num_filters_2 = 30
 # Kernel size for all convolutions
-kernel_size = 5
+kernel_size = 3
 # Pool size for all pooling
 pool_size = 2
 
@@ -52,6 +49,7 @@ lines = []
 seen_chars = set()
 with open('31100.txt', encoding='latin-1') as text_file:
     for line in text_file:
+        line = line.rstrip('\r\n')
         if len(line) > 0:
             if len(line) > max_len:
                 line = line[:max_len]
@@ -59,6 +57,8 @@ with open('31100.txt', encoding='latin-1') as text_file:
             # record which characters appear in input
             for c in line:
                 seen_chars.add(c)
+            if len(lines) >= 20000:
+                break #TODO remove limit
 # Shuffle input for good measure
 random.shuffle(lines)
 
@@ -66,8 +66,6 @@ random.shuffle(lines)
 char_to_index = dict(zip(seen_chars, range(0, len(seen_chars))))
 index_to_char = {i: c for c, i in char_to_index.items()}
 num_chars = len(char_to_index)
-
-print('Text has {} chars'.format(num_chars))
 
 num_lines = len(lines)
 # One-hot encode the input
@@ -80,16 +78,13 @@ for i in range(0, num_lines):
     for j in range(line_len, max_len):
         one_hot[i, j, char_to_index.get(' ')] = 1
 
-# See https://blog.keras.io/building-autoencoders-in-keras.html
-# for more background on convolutional autoencoders in Keras
-
 model = Sequential()
 with tf.device(device1):
     # Learn embedding from one-hot encoding to vector space
     model.add(Dense(name='embedding',
                     input_shape=(max_len, num_chars),
                     units=embedding_dim,
-                    activation='relu'))
+                    activation=None))
     # Encode convolution + pool 1
     model.add(Conv1D(name='encoder_conv_1',
                      filters=num_filters_1,
@@ -106,39 +101,24 @@ with tf.device(device1):
                      activation='relu'))
     model.add(MaxPooling1D(name='encoder_pool_2',
                            pool_size=pool_size))
-    # Encode convolution + pool 3
-    model.add(Conv1D(name='encoder_conv_3',
-                     filters=num_filters_3,
-                     kernel_size=kernel_size,
-                     padding='same',
-                     activation='relu'))
-    model.add(MaxPooling1D(name='encoder_pool_3',
-                           pool_size=pool_size))
 with tf.device(device2):
-    # Decode convolution + pool 3
-    model.add(Conv1D(name='decoder_conv_3',
-                     filters=num_filters_3,
-                     kernel_size=kernel_size,
-                     padding='same',
-                     activation='relu'))
-    model.add(UpSampling1D(name='decoder_unpool_3',
-                           size=pool_size))
     # Decode convolution + pool 2
+    model.add(UpSampling1D(name='decoder_unpool_2',
+                           size=pool_size))
     model.add(Conv1D(name='decoder_conv_2',
                      filters=num_filters_2,
                      kernel_size=kernel_size,
                      padding='same',
                      activation='relu'))
-    model.add(UpSampling1D(name='decoder_unpool_2',
-                           size=pool_size))
     # Decode convolution + pool 1
+    model.add(UpSampling1D(name='decoder_unpool_1',
+                           size=pool_size))
     model.add(Conv1D(name='decoder_conv_1',
                      filters=num_filters_1,
                      kernel_size=kernel_size,
                      padding='same',
                      activation='relu'))
-    model.add(UpSampling1D(name='decoder_unpool_1',
-                           size=pool_size))
+
     # Learn mapping back from vector space to one-hot encoding
     # TODO weight tying?
     model.add(Dense(name='unembedding',
@@ -148,43 +128,65 @@ with tf.device(device2):
 model.summary()
 
 # Compile and fit model
-model.compile(optimizer=RMSprop(lr=0.01),
+model.compile(optimizer='adadelta',
               loss='categorical_crossentropy',
               metrics=['acc'])
 
-history = model.fit(one_hot, one_hot,
-                    epochs=200,
-                    batch_size=32,
-                    verbose=2)
+model.fit(one_hot, one_hot,
+          epochs=20,
+          batch_size=64,
+          verbose=2)
 
+lines_to_predict = 5
 # Round-trip encode-decode the first line
-predicted = model.predict(one_hot[:1])
-
+predicted = model.predict(one_hot[:lines_to_predict])
 
 # Examine the activations in the first encoder convolutional layer
-activation_model = Model(inputs=[model.input], outputs=[model.layers[1].output])
-activations = activation_model.predict(one_hot[:1])[0]
+activation_model = Model(inputs=[model.input],
+                         outputs=[model.layers[1].output, model.layers[3].output])
+activations = activation_model.predict(one_hot[:lines_to_predict])
+activations_1 = activations[0]
+activations_2 = activations[1]
 
 # Find scale of activations
-min_act = np.min(activations)
-act_range = np.max(activations) - min_act
+min_act_1 = np.min(activations_1)
+act_range_1 = np.max(activations_1) - min_act_1
+min_act_2 = np.min(activations_2)
+act_range_2 = np.max(activations_2) - min_act_2
+
+# Number of different activation strengths to display differently
 levels = 5
-cutoffs = list(map(lambda i: min_act + act_range * (i / (levels - 1)), range(0, levels)))
-print(cutoffs)
+# Max activations for each activation strength bucket
+cutoffs_1 = list(map(lambda i: min_act_1 + act_range_1 * (i / (levels - 1)), range(0, levels)))
+cutoffs_2 = list(map(lambda i: min_act_2 + act_range_2 * (i / (levels - 1)), range(0, levels)))
+# Chars to use to render each activation
 display_chars = " .-=*@"
 assert len(display_chars) == levels + 1
 
-def print_activation(f):
+
+def print_activation(f, cutoffs):
     for i in range(0, levels):
         if f <= cutoffs[i]:
             return display_chars[i]
     return display_chars[-1]
-  
-# Print line, and its decoded form, and pretty-print neurons
-decoded = ''.join(map(lambda a: index_to_char.get(a), np.argmax(predicted[0], axis=1)))
-  
-for neuron in range(0, num_filters_1):
-    print(lines[0])
-    print(decoded)
-    print(''.join(map(print_activation, activations[:,neuron])))
+
+# How many different neurons/filtering in each conv layer to show
+filters_to_show = 10
+
+# Show first conv layer activations
+for l in range(0, lines_to_predict):
+    print(lines[l])
+    for n in range(0, filters_to_show):
+        print(''.join(map(lambda a: print_activation(a, cutoffs_1), activations_1[l,:,n])))
+    print(''.join(map(lambda a: index_to_char.get(a), np.argmax(predicted[l], axis=1))))
+    print()
+
+# Show second conv layer activations
+for l in range(0, lines_to_predict):
+    print(lines[l])
+    for n in range(0, filters_to_show):
+        # * pool_size because next conv layer follows downsampling from pool
+        # Each activation is a function of multiple inputs
+        print(''.join(map(lambda a: print_activation(a, cutoffs_2) * pool_size, activations_2[l,:,n])))
+    print(''.join(map(lambda a: index_to_char.get(a), np.argmax(predicted[l], axis=1))))
     print()
